0089225125622ba33b1921bd10a8caf3f647889d
Satvik1912 <pandeysatvik2002@gmail.com>
2025-02-11 14:21:37 +0530

    intial commit
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/cars24/lms_backend/LmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars24.lms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
